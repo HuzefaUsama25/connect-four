8c5ceeaa434f80620bea758d6f0c5528b829fe6b
HuzefaUsama25 <huzefausama25@gmail.com>
2021-11-27 12:05:57 +0500

    Initial Commit

--- computer_player.py
+++
@@ -1,8 +0,0 @@
-from connect4 import connect4
-
-class computer:
-    def __init__(self):
-        pass
-        
-        
-        
--- main.py
+++ main.py
@@ -2,28 +2,38 @@
 
 import os
 from connect4 import connect4
-from computer_player import computer
-
+import random
                         
+def random_col():
+    return random.randint(1,7)
+
+
 
 def main():
     game = connect4()
     colors = game.colors
     color = colors[0]
+
     while True:
         try:
-            if color==colors[0]:color=colors[1]
-            elif color==colors[1]:color=colors[0]
+            
             os.system("clear")
             game.show_board()
-            column_num = int(input(f"\n\n{color}: ")) - 1
+            if color==colors[0]:
+                color=colors[1]
+                column_num = int(input(f"\n\n{color}: ")) - 1
+            
+            elif color==colors[1]:
+                color=colors[0]
+                column_num = random_col()
+            
             game.add_ball(column_num, colors.index(color))
             win = game.check_win()
             if win != None:
                 os.system("clear")
                 game.show_board()
                 print(f"\n{win} Wins!")
-                break
+                exit()
         except Exception as e:
             print(e)
             continue
